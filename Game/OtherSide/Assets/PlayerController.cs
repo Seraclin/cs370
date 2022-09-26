1b5cbf9c01a160f5d586bb5ed20092e372ac8578
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2022-09-26 09:48:55 -0400

    Added comments to PlayerControls
    Also gave the player their sprite and added a "Physics Obstacle". We don't need the physics obstacle, I just added it if we ever want it.

--- Game/OtherSide/Assets/PlayerController.cs
+++ Game/OtherSide/Assets/PlayerController.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] Vector2 input;
-    
-    [SerializeField] float speedMultiplier;
-    [SerializeField] float speed = 2;
-    [SerializeField] float topSpeed;
-    [SerializeField] bool isMoving = false;
+    /*IMPORTANT So in Unity, it's good practice to not use public variables and instead use
+     * [SerializeField]. SerializeField variables can be changed via the Unity Engine itself.
+     * So like for example, we can set the speed to another number without having to compile 
+     * and run the code -JC
+
+    /*Instead of using 4 if statements, this allows for multiple different control setups
+     *like Switch controllers, keyboards, etc without having to code them all separately.
+     *Yall might need to download the input manager plugin (forgot the exact name) -JC
+     */
+    Vector2 input; //Input by players (Example: The "Up" button) -JC
+    [SerializeField] float speed;
+    [SerializeField] bool isMoving = false; //Used for grid-based movement later on
+
+    /*This is an InEngine layer which is pretty much is everything the player can collide with
+     * For example: walls, enemies, etc.
+     * Things that are not in this layer are things players cannot collide with
+     * Fore example: The sky, clouds, etc -JC
+     */
     [SerializeField] LayerMask Collidables;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -21,17 +34,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        input.x = Input.GetAxisRaw("Horizontal");
+        //Built-in Unity functions for player inputs
+        input.x = Input.GetAxisRaw("Horizontal"); 
         input.y = Input.GetAxisRaw("Vertical");
+
+        //If input!=Zero then that means a button is being pressed -JC
         if (input != Vector2.zero && !isMoving)
         {
             var targetPos = transform.position;
-            targetPos.x += input.x / 16;
+            targetPos.x += input.x / 16; //I'm assuming the game will be 16X16pixels here.
             targetPos.y += input.y / 16;
-            if (CanWalk(targetPos))
-            StartCoroutine(Move(targetPos, input.x, input.y));
+            if (CanWalk(targetPos)) {
+                /*The reason I use couroutine/IEnumerator instead of just moving on update is because
+                 * 1. We are doing grid based movement. Each player should move a pixel at a time
+                 * 2. Update runs every frame, which is not as efficient -JC
+                 */
+                StartCoroutine(Move(targetPos, input.x, input.y));
+                 }
         }
     }
+    //This just is a way to make each player move 1 pixel at a time -JC
     IEnumerator Move(Vector3 targetPos, float inputx, float inputy)
     {
         isMoving = true;
@@ -43,9 +65,12 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
         isMoving = false;
     }
-    bool CanWalk(Vector3 targetPos) //This checks if there's an object in the way
+    bool CanWalk(Vector3 targetPos) //This checks if there's an object in the way -JC
     {
-        if (Physics2D.OverlapCircle(targetPos, 0.5f,Collidables) != null){
+        /*I use a circular collider instead of a square one so that the players won't get stuck
+         * on corners -JC
+         */
+        if (Physics2D.OverlapCircle(targetPos, 0.4f,Collidables) != null){
             return false;
         }
         
