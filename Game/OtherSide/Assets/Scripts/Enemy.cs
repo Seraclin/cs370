0faca2b5d8055e9dbaec506b4943259818e73046
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2022-10-03 09:23:25 -0400

    Mispelling

--- Game/OtherSide/Assets/Scripts/Enemy.cs
+++ Game/OtherSide/Assets/Scripts/Enemy.cs
@@ -81,7 +81,7 @@ public class Enemy : MonoBehaviour
 
 
     //The green circle collider around the enemy is the enemy's range -JC
-    private void OnTriggerEnter(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
    
         if (collision.gameObject.tag =="Player")
