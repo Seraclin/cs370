e0d5ca1bf526cfb62ec22e802dd93a78140b4fd6
Samantha Lin <sampster8@gmail.com>
2022-10-29 14:17:24 -0400

    Revert "Added Ranged Ability"
    This reverts commit 140f2ece5e5e3622f1d793537908fc3ce9c6e15e.

--- Game/OtherSide/Assets/Scripts/RangedAbility.cs
+++ Game/OtherSide/Assets/Scripts/RangedAbility.cs
@@ -13,7 +13,6 @@ public class RangedAbility : Ability
 {
     // RangedAbility inherits attributes from Ability object
 
-
     // Extra defined variables for RangedAbility
     public Sprite projectile; // sprite for the projectile
     public int projectileRange; // how far the projectile can travel in pixels
@@ -22,35 +21,10 @@ public class RangedAbility : Ability
     // public float projectileDuration; // how long the projectile exists, don't need this if we have an 'activeTime' in the base Ability class
     // Two options: calculate predetermined distance by having projectile travel from start to projectileRange at 'speed', or we can have it only last for a 'duration' and it travels at 'speed'
 
-    private GameObject rangeInstance; 
-    private Transform spawnPosition; 
-    private GameObject cloneSkillPrefab; 
-    private  SpriteRenderer m_SpriteRenderer; 
-    private Rigidbody rb; 
-
     public override void Activate(GameObject parent) // define ability behavior, parent is the GameObject the script is attached to
     {
         // TODO: Put code for shooting stuff from player's position, the "AbilityHolder" script should call this
-
         Debug.Log("Ranged Ability activated");
-
-        rangeInstance = new GameObject();
-        rangeInstance.AddComponent<SpriteRenderer>();
-        rangeInstance.AddComponent<Rigidbody>();
-
-
-        rb = rangeInstance.GetComponent<Rigidbody>();
-        m_SpriteRenderer = rangeInstance.GetComponent<SpriteRenderer>();
-        
-        m_SpriteRenderer.sprite = projectile; 
-
-        rangeInstance.transform.position = parent.transform.position; 
-
-        rb.useGravity = false;
-
-        rb .velocity = new Vector2(-projectileSpeed, 0); 
-
-        Destroy(rangeInstance, projectileRange / projectileSpeed);
     }
 
 
