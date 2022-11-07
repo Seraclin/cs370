ef83f7662532e9716817dcea99a40e96137af9b5
Sirious1y <jamess0378@gmail.com>
2022-11-07 10:45:51 -0500

    Restore Ranged Enemy Prefab and Related Changes
    Check Enemy with Ranged Ability for details

--- Game/OtherSide/Assets/Scripts/Bullet.cs
+++ Game/OtherSide/Assets/Scripts/Bullet.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] int damage; //this is the amount of damage done by the weapon
-    Rigidbody2D rb;
-    
+    [SerializeField] float duration;
+    void FixedUpdate()
+    {
+        if (duration > 0)
+        {
+            duration -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (gameObject.tag != collision.gameObject.tag + "Ability")
         {
-            
+
 
             if (collision.gameObject.tag == "Enemy")
             {
-                
+
                 Enemy eScript = collision.gameObject.GetComponent<Enemy>();
-                
                 eScript.ChangeHealth(damage);
                 Destroy(gameObject);
-               
+
             }
             else if (collision.gameObject.tag == "Wall")
             {
