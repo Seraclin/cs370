992b085e266960695e51fc85636ee6d5c6af8f8e
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2022-11-09 09:06:06 -0500

    Made start screen like powerpoint
    No more James Corden

--- Game/OtherSide/Assets/MenuController.cs
+++ Game/OtherSide/Assets/MenuController.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] private string versionName = "0.1";
+    [SerializeField] private GameObject usernameMenu;
+    [SerializeField] private GameObject connectPanel;
+
+    [SerializeField] private InputField usernameInput;
+    [SerializeField] private InputField createGameInput;
+    [SerializeField] private InputField joinGameInput;
+
+    [SerializeField] private GameObject startBtn;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    private void Start()
+    {
+        usernameMenu.SetActive(true);
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void ChangeUsernameInput()
+    {
+        if (usernameInput.text.Length < 3)
+        {
+            startBtn.SetActive(false);
+
+        }
+        else
+        {
+            startBtn.SetActive(true);
+        }
+    }
+
+    public void SetUsername()
+    {
+        usernameMenu.SetActive(false);
+        PhotonNetwork.NickName = usernameInput.text;
     }
 }
