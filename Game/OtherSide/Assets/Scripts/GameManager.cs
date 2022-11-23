c2c905fa28bad3dc282787e253a1597253138b53
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2022-11-23 11:57:25 -0600

    Added Fire Effect to Start Screen

--- Game/OtherSide/Assets/Scripts/GameManager.cs
+++ Game/OtherSide/Assets/Scripts/GameManager.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
-public class GameManager : MonoBehaviour
+using UnityEngine.UI;
+public class GameManager : MonoBehaviourPunCallbacks
 {
     public GameObject playerPrefab;
     public GameObject GameCanvas;
     public GameObject SceneCamera;
+    public Text LobbyText;
     // Start is called before the first frame update
     void Awake()
     {
         GameCanvas.SetActive(true);
+        LobbyText.text = "";
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            LobbyText.text += player.NickName + "\n";
+        }
     }
-
+    public override void OnPlayerEnteredRoom(Player newPlayer){
+        Debug.Log(newPlayer.NickName + "Has Entered Room");
+        LobbyText.text = "";
+        foreach (Player player in PhotonNetwork.PlayerList) {
+            LobbyText.text += player.NickName + "\n";
+        }
+        }
     // Update is called once per frame
     void Update()
     {
