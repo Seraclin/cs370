d8b5d9d4afb622a46eb05ed7c9648e50bbc2d87b
Sirious1y <jamess0378@gmail.com>
2022-12-04 03:04:03 -0500

    Abilities for boss
    2 new abilities for boss:
    - splitted fireball: shoots fireballs in 3 directions
    - spawn: spawn zombies between boss and player
    
    Modified boss's prefab:
    - shrink it's hitbox and collider to reduce the chance when it gets stuck with other enemies
    
    Change all enemies' colliders to capsule colliders:
    - did not solve the problem where enemies get stuck on each other

---
+++ Game/OtherSide/Assets/Scripts/Abilities/Spawn.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Spawn", menuName = "Scripts/Spawn")]
+public class Spawn : Ability
+{
+    public GameObject enemyInstance; // The prefab of melee (currently it's a placeholder from the existing sprites)
+    private Transform spawnPosition; // The position of generating the meleeInstance
+    private GameObject cloneSkillPrefab; // Reference to the generated melee object
+
+    private Vector3 direction;
+
+    [SerializeField] GameObject spawnParticle; // particle
+
+    /* Activate is called when the skill is active
+     * define ability behavior: generate an object with a collider as the hitbox of the melee   
+     */
+    public override void Activate(GameObject parent)
+    {
+        // Debug.Log("Melee Ability activated");
+        FindObjectOfType<AudioManager>().Play("meleeAttack");
+
+        spawnPosition = parent.transform;
+        Vector3 displacement = new Vector3(0, 0, 0);
+
+        if (parent.tag == "Enemy")
+        {
+            Vector3 playerPosition = parent.GetComponent<Enemy>().player.GetComponent<Transform>().position;
+            displacement = playerPosition - spawnPosition.position;
+            direction = displacement.normalized;
+            if (Vector3.Distance(spawnPosition.position + 10 * direction, playerPosition) > 3)
+            {
+                cloneSkillPrefab = Instantiate(enemyInstance, spawnPosition.position + 10 * direction, spawnPosition.rotation);
+                GameObject phit = Instantiate(spawnParticle, cloneSkillPrefab.transform);
+                cloneSkillPrefab.GetComponent<Enemy>().player = parent.GetComponent<Enemy>().player;
+            }
+        }
+
+       
+    }
+
+    /* Deactivate is called when the skill is cooldown
+    * destroy the melee object
+    */
+    public override void Deactivate(GameObject parent) 
+    {
+        // Debug.Log("Melee Ability deactivated");
+        // Destroy(cloneSkillPrefab);
+    }
+}
---
+++ Game/OtherSide/Assets/Scripts/Abilities/SplittingBullet.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New_SplittingBullet", menuName = "Scripts/SplittingBullet")]
+public class SplittingBullet : Ability
+{
+    public GameObject RangeInstance; // prefab for the projectile
+    private Transform spawnPosition; // position of generating the instance
+    //public int projectileRange; // how far the projectile can travel in pixels
+    private GameObject cloneSkillPrefab; // store generated ability object
+    private GameObject cloneSkillPrefab2;
+    private GameObject cloneSkillPrefab3;
+    private Vector3 direction;
+    private Vector3 direction2;
+    private Vector3 direction3;
+    public float projectileSpeed; // how fast the projectile travels across the range, it's actually the force
+    //public float projectileSize; // how big the projectile is
+    // public float projectileDuration; // how long the projectile exists, don't need this if we have an 'activeTime' in the base Ability class
+    // Two options: calculate predetermined distance by having projectile travel from start to projectileRange at 'speed', or we can have it only last for a 'duration' and it travels at 'speed'
+
+    public override void Activate(GameObject parent)
+    {
+
+        // Debug.Log("Range Ability activated");
+        FindObjectOfType<AudioManager>().Play("fireballAttack");
+
+        spawnPosition = parent.transform;
+        Vector3 displacement = new Vector3(0, 0, 0);
+
+        if (parent.tag == "Player")
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = Camera.main.transform.position.z * -1;
+            displacement = Camera.main.ScreenToWorldPoint(mousePosition) - spawnPosition.position;
+        }
+        else if (parent.tag == "Enemy")
+        {
+            Vector3 playerPosition = parent.GetComponent<Enemy>().player.GetComponent<Transform>().position;
+            displacement = playerPosition - spawnPosition.position;
+        }
+
+
+        direction = displacement.normalized;
+        direction2 = Quaternion.Euler(0, 0, 30) * direction;
+        direction3 = Quaternion.Euler(0, 0, -30) * direction;
+
+        cloneSkillPrefab = Instantiate(RangeInstance, spawnPosition.position + direction, spawnPosition.rotation);
+        cloneSkillPrefab2 = Instantiate(RangeInstance, spawnPosition.position + direction, spawnPosition.rotation);
+        cloneSkillPrefab3 = Instantiate(RangeInstance, spawnPosition.position + direction, spawnPosition.rotation);
+
+        cloneSkillPrefab.tag = parent.tag + "Ability";
+        cloneSkillPrefab2.tag = parent.tag + "Ability";
+        cloneSkillPrefab3.tag = parent.tag + "Ability";
+
+        float rotZ = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        cloneSkillPrefab.transform.rotation = Quaternion.Euler(0f, 0f, -rotZ - 90);
+        cloneSkillPrefab2.transform.rotation = Quaternion.Euler(0f, 0f, -rotZ - 60);
+        cloneSkillPrefab3.transform.rotation = Quaternion.Euler(0f, 0f, -rotZ - 120);
+
+        cloneSkillPrefab.GetComponent<Rigidbody2D>().AddForce(direction * projectileSpeed, ForceMode2D.Force);
+        cloneSkillPrefab2.GetComponent<Rigidbody2D>().AddForce(direction2 * projectileSpeed, ForceMode2D.Force);
+        cloneSkillPrefab3.GetComponent<Rigidbody2D>().AddForce(direction3 * projectileSpeed, ForceMode2D.Force);
+
+        cloneSkillPrefab.GetComponent<Bullet>().damage = damage;
+        cloneSkillPrefab.GetComponent<Bullet>().maker = parent;
+
+        cloneSkillPrefab2.GetComponent<Bullet>().damage = damage;
+        cloneSkillPrefab2.GetComponent<Bullet>().maker = parent;
+
+        cloneSkillPrefab3.GetComponent<Bullet>().damage = damage;
+        cloneSkillPrefab3.GetComponent<Bullet>().maker = parent;
+
+    }
+
+    /* Deactivate is called when the skill is cooldown
+    * destroy the melee object
+    */
+    public override void Deactivate(GameObject parent) // 
+    {
+        Debug.Log("Range Ability deactivated");
+        //Destroy(cloneSkillPrefab);
+    }
+
+
+
+}
