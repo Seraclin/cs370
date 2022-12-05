04149b03a886c249fa86a2aebac297ba764ea604
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2022-12-05 02:45:13 -0500

    hopefully fixed generation
    maybe not

--- Game/OtherSide/Assets/Scripts/MapRuntimeGenerator.cs
+++ Game/OtherSide/Assets/Scripts/MapRuntimeGenerator.cs
@@ -5,16 +5,50 @@ using UnityEngine.Events;
 using Photon.Pun;
 using Photon.Realtime;
 
+
 public class MapRuntimeGenerator : MonoBehaviour
 {
     public UnityEvent OnStart;
     public PhotonView pv;
+
+    [SerializeField] GameObject Generator1;
+    [SerializeField] GameObject Generator2;
+    [SerializeField] GameObject Generator3;
+    [SerializeField] GameObject Generator4;
+    [SerializeField] GameObject Generator5;
+    [SerializeField] GameObject Generator6;
+    [SerializeField] GameObject Generator7;
+    [SerializeField] GameObject Generator8;
+    [SerializeField] GameObject Generator9;
     // Start is called before the first frame update
     void Start()
     {
 
         if (PhotonNetwork.IsMasterClient)
-        OnStart?.Invoke();
-    }
+        {
+            OnStart?.Invoke();
+        }
+        else
+        {
 
+        }
+    }
+    private void Awake()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            Generator1.SetActive(false);
+            Generator2.SetActive(false);
+            Generator3.SetActive(false);
+            Generator4.SetActive(false);
+            Generator5.SetActive(false);
+            Generator6.SetActive(false);
+            Generator7.SetActive(false);
+            Generator8.SetActive(false);
+            Generator9.SetActive(false);
+
+
+        }
+
+    }
 }
